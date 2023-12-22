7876985daa26b4f6e68f9cd56d80d7349adab95a
Brett Stalcup <brettstalcup@gmail.com>
2023-12-22 13:53:21 -0600

    .

--- website/views.py
+++ website/views.py
@@ -3,4 +3,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    
     return render(request, 'home.html', {})
+
+
+
+def logout_user(request):
+    pass
