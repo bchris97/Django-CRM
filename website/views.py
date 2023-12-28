5694699202f1d964a1b86ffaebcdee0eacf2e265
Brett Stalcup <brettstalcup@gmail.com>
2023-12-28 16:55:10 -0600

    -

--- website/views.py
+++ website/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+    # Check if user is logged in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+            return redirect('home')
     
     return render(request, 'home.html', {})
 
 
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You are now logged out')
+    return redirect('home')
