6860ec967fd975257c7fc4cbd93445493bb51919
Brett Stalcup <brettstalcup@gmail.com>
2023-12-30 10:30:52 -0600

    .

--- virt/bin/activate.fish
+++ virt/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/brett/Desktop/dcrm/dcrm/virt"
+set -gx VIRTUAL_ENV "/Users/brett/Desktop/Projects/dcrm/dcrm/virt"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
